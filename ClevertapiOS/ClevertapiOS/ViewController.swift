d67ede6d5fea1e1f94192f946c4f2b125a4c6e89
hadiaandarleanplum <hadia.andar@leanplum.com>
2022-07-28 10:51:59 -0700

    update

---
+++ ClevertapiOS/ClevertapiOS/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  ClevertapiOS
+//
+//  Created by Hadia Andar on 7/18/22.
+//
+
+import UIKit
+import CleverTapSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let profile = [
+            //Update pre-defined profile properties
+            "Name": "Apple Montana",
+            "Email": "Apple.montana@gmail.com",
+            //Update custom profile properties
+            "Plan type": "Silver",
+            "Favorite Food": "Pizza"
+        ]
+
+        CleverTap.sharedInstance()?.onUserLogin(profile)
+        
+        CleverTap.sharedInstance()?.recordEvent("User Logged In");
+    }
+    
+    func application(application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        CleverTap.sharedInstance()?.setPushToken(deviceToken as Data)
+    }
+
+
+}
+
---
+++ ClevertapiOS/Pods/Target Support Files/Pods-ClevertapiOSTests/Pods-ClevertapiOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ClevertapiOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_ClevertapiOSTests
+@end
